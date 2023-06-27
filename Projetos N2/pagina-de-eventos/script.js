607625ed28e43daedf91b90ae25a27b7aaa45322
CKeven173 <cawankeven@hotmail.com>
2023-06-27 11:29:41 -0300

    cronometro funcionando

--- Projetos N2/pagina-de-eventos/script.js
+++ Projetos N2/pagina-de-eventos/script.js
@@ -3,11 +3,31 @@ const hoursEl = document.getElementById("hours");
 const minsEl = document.getElementById("mins");
 const secondsEl = document.getElementById("seconds");
 
-const Show = "17 Nov 2023";
+const taylorShow = "17 Nov 2023";
 
-function countdown();
+function countdown(){
 
-    const ShowDate = new Date(Show);
+    const taylorShowDate = new Date(taylorShow);
     const currentDate = new Date();
 
-    const totalSeconds = (newShowDate - currentDate) / 1000;
+    const totalSeconds = (taylorShowDate - currentDate) / 1000;
+
+    const days = Math.floor(totalSeconds / 3600 / 24);
+    const hours = Math.floor(totalSeconds / 3600) % 24 ;
+    const mins = Math.floor(totalSeconds / 60) % 60;
+    const seconds = Math.floor(totalSeconds) % 60;
+
+    daysEl.innerHTML = days;
+    hoursEl.innerHTML = formatTime(hours);
+    minsEl.innerHTML = formatTime(mins);
+    secondsEl.innerHTML = formatTime(seconds);
+
+}
+
+function formatTime(time){
+    return time < 10 ? `0${time}` : time;
+}
+
+countdown();
+
+setInterval(countdown, 1000);
