ab39e83e0da4b29bf337b3d8c5cb98c689ee032e
Matheus Germano <germanomatheus40@hotmail.com>
2023-06-27 12:31:15 -0300

    API validate

--- Projetos N2/dashboard/script.js
+++ Projetos N2/dashboard/script.js
@@ -1,24 +1,18 @@
-window.addEventListener('DOMContentLoaded', () => {
-    const table = document.getElementById('table');
-    const tbody = table.querySelector('tbody');
-  
-    const apiKey = 'live_cabac4fb76ba295464163490b78291';
-    const url = 'https://api.api-futebol.com.br/v1/campeonatos/10/tabela';
-  
-    const requestOptions = {
-      headers: {
-        'Authorization': `Bearer ${apiKey}`
-      }
-    };
-  
-    fetch(url, requestOptions)
-  .then(response => {
-    if (!response.ok) {
-      throw new Error('Erro ao obter os dados da API.');
-    }
-    return response.json();
-  })
+const apiKey = 'live_cabac4fb76ba295464163490b78291';
+const url = 'https://api.api-futebol.com.br/v1/campeonatos/10/fases/317';
+
+const table = document.getElementById('table');
+const tbody = table.querySelector('tbody');
+
+fetch(url, {
+  headers: {
+    'Authorization': `Bearer ${apiKey}`
+  }
+})
+  .then(response => response.json())
   .then(data => {
+    console.log(data);
+    
     if (data && data.tabela) {
       data.tabela.forEach((equipe) => {
         const row = document.createElement('tr');
@@ -34,5 +28,3 @@ window.addEventListener('DOMContentLoaded', () => {
   .catch(error => {
     console.error('Ocorreu um erro ao obter os dados da API:', error);
   });
-  });
-  
