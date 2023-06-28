89732bf7b9316d3737ff583e5108bc59d3aff48d
CKeven173 <cawankeven@hotmail.com>
2023-06-27 23:22:21 -0300

    responsividade

--- Projetos N2/dashboard/script.js
+++ Projetos N2/dashboard/script.js
@@ -16,13 +16,13 @@ function closeSidebar() {
   }
 }
 
-// APEXCHARTS graficos:
-
+// APEXCHARTS, biblioteca de tabelas:
+// tabela em colunas:
 
 var barChartOptions = {
   series: [{
     data: [10, 8, 6, 4, 2],
-    name: "Products",
+    name: "Produtos",
   }],
   chart: {
     type: "bar",
@@ -84,7 +84,7 @@ var barChartOptions = {
     theme: "dark",
   },
   xaxis: {
-    categories: ["Laptop", "Phone", "Monitor", "Headphones", "Camera"],
+    categories: ["Notebook", "Celular", "Monitor", "Headphones", "Camera"],
     title: {
       style: {
         color: "#f5f7ff",
@@ -106,7 +106,7 @@ var barChartOptions = {
   },
   yaxis: {
     title: {
-      text: "Count",
+      text: "Vendas",
       style: {
         color:  "#f5f7ff",
       },
@@ -130,3 +130,121 @@ var barChartOptions = {
 
 var barChart = new ApexCharts(document.querySelector("#bar-chart"), barChartOptions);
 barChart.render();
+
+//tabela em ondas:
+
+var areaChartOptions = {
+  series: [{
+    name: "Pedidos",
+    data: [31, 40, 28, 51, 42, 109, 100],
+  }, {
+    name: "Vendas",
+    data: [11, 32, 45, 32, 34, 52, 41],
+  }],
+  chart: {
+    type: "area",
+    background: "transparent",
+    height: 350,
+    stacked: false,
+    toolbar: {
+      show: false,
+    },
+  },
+  colors: ["#00ab57", "#d50000"],
+  labels: ["Jan", "Fev", "Mar", "Abr", "Mai", "Jun", "Jul"],
+  dataLabels: {
+    enabled: false,
+  },
+  fill: {
+    gradient: {
+      opacityFrom: 0.4,
+      opacityTo: 0.1,
+      shadeIntensity: 1,
+      stops: [0, 100],
+      type: "vertical",
+    },
+    type: "gradient",
+  },
+  grid: {
+    borderColor: "#55596e",
+    yaxis: {
+      lines: {
+        show: true,
+      },
+    },
+    xaxis: {
+      lines: {
+        show: true,
+      },
+    },
+  },
+  legend: {
+    labels: {
+      colors: "#f5f7ff",
+    },
+    show: true,
+    position: "top",
+  },
+  markers: {
+    size: 6,
+    strokeColors: "#1b2635",
+    strokeWidth: 3,
+  },
+  stroke: {
+    curve: "smooth",
+  },
+  xaxis: {
+    axisBorder: {
+      color: "#55596e",
+      show: true,
+    },
+    axisTicks: {
+      color: "#55596e",
+      show: true,
+    },
+    labels: {
+      offsetY: 5,
+      style: {
+        colors: "#f5f7ff",
+      },
+    },
+  },
+  yaxis: 
+  [
+    {
+      title: {
+        text: "Pedidos",
+        style: {
+          color: "#f5f7ff",
+        },
+      },
+      labels: {
+        style: {
+          colors: ["#f5f7ff"],
+        },
+      },
+    },
+    {
+      opposite: true,
+      title: {
+        text: "Vendas",
+        style: {
+          color:  "#f5f7ff",
+        },
+      },
+      labels: {
+        style: {
+          colors: ["#f5f7ff"],
+        },
+      },
+    },
+  ],
+  tooltip: {
+    shared: true,
+    intersect: false,
+    theme: "dark",
+  }
+};
+
+var areaChart = new ApexCharts(document.querySelector("#area-chart"), areaChartOptions);
+areaChart.render();
