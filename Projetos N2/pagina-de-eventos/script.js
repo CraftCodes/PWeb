a7007807f9d91e53eaf56f15cf12a1ecb26d4bb3
mathgermano <germanomatheus40@hotmail.com>
2023-06-28 16:48:22 -0300

    js comments & dashboard updates

--- Projetos N2/pagina-de-eventos/script.js
+++ Projetos N2/pagina-de-eventos/script.js
@@ -1,25 +1,25 @@
 const daysEl = document.getElementById("days");
-const hoursEl = document.getElementById("hours");
+const hoursEl = document.getElementById("hours"); // elementos do cronômetro sendo declarados
 const minsEl = document.getElementById("mins");
 const secondsEl = document.getElementById("seconds");
 
-const taylorShow = "20 Jul 2023";
+const taylorShow = "20 Jul 2023"; // data de referência ao cronômetro
 
 function countdown(){
 
-    const taylorShowDate = new Date(taylorShow);
+    const taylorShowDate = new Date(taylorShow); // atribuição da data relativa a data pré-definida
     const currentDate = new Date();
 
     const totalSeconds = (taylorShowDate - currentDate) / 1000;
 
     const days = Math.floor(totalSeconds / 3600 / 24);
-    const hours = Math.floor(totalSeconds / 3600) % 24 ;
+    const hours = Math.floor(totalSeconds / 3600) % 24 ; // cálculos necessários para dias, horas, minutos e segundos
     const mins = Math.floor(totalSeconds / 60) % 60;
     const seconds = Math.floor(totalSeconds) % 60;
 
     daysEl.innerHTML = days;
     hoursEl.innerHTML = formatTime(hours);
-    minsEl.innerHTML = formatTime(mins);
+    minsEl.innerHTML = formatTime(mins); // inserção dos dados ao HTML
     secondsEl.innerHTML = formatTime(seconds);
 
 }
--- Projetos N2/site-api/script.js
+++ Projetos N2/site-api/script.js
@@ -1,27 +1,27 @@
 window.addEventListener('DOMContentLoaded', function() {
 
-  const apiKey = 'live_cabac4fb76ba295464163490b78291';
+  const apiKey = 'live_cabac4fb76ba295464163490b78291'; // api key responsável por autorizar a requisição de dados (USO LIMITADO!!!)
 
   const headers = {
     'Content-Type': 'application/json',
-    'Authorization': `Bearer ${apiKey}`
+    'Authorization': `Bearer ${apiKey}` // será usado no fetch para puxar os dados da API e colocar no site
 };
 
-  fetch('https://api.api-futebol.com.br/v1/campeonatos/10/fases/317', { headers })
+  fetch('https://api.api-futebol.com.br/v1/campeonatos/10/fases/317', { headers }) // url da api, onde os dados estão armazenados
       .then(response => response.json())
       .then(data => {
           console.log(data);
 
           const tabela = data.tabela;
           const table = document.createElement('table');
-          const thead = document.createElement('thead');
+          const thead = document.createElement('thead'); // criação dos atributos em html para a tabela, separando em "divs"
           const tbody = document.createElement('tbody');
 
-          const headers = ['Posição', 'Time', 'Escudo', 'Pontos', 'Jogos', 'Vitórias'];
+          const headers = ['Posição', 'Time', 'Escudo', 'Pontos', 'Jogos', 'Vitórias']; // elementos do cabeçalho da tabela, demonstrando o que será mostrado
 
           const headerRow = document.createElement('tr');
           headers.forEach(header => {
-              const th = document.createElement('th');
+              const th = document.createElement('th'); // criar os elementos onde os dados de cada time serão postos
               th.textContent = header;
               headerRow.appendChild(th);
           });
@@ -34,40 +34,40 @@ window.addEventListener('DOMContentLoaded', function() {
               const { time, pontos, jogos, vitorias } = posicao;
 
               const posicaoTd = document.createElement('td');
-              posicaoTd.textContent = posicao.posicao;
+              posicaoTd.textContent = posicao.posicao; // elemento posição, numerado de 1 a 20
               tr.appendChild(posicaoTd);
 
               const timeTd = document.createElement('td');
               const nomeTime = time.nome_popular || time.nome;
-              timeTd.textContent = nomeTime;
+              timeTd.textContent = nomeTime; // elemento nome do time, identificando o time e sua respectiva posição
               tr.appendChild(timeTd);
 
               const escudoTd = document.createElement('td');
               const escudoImg = document.createElement('img');
-              escudoImg.src = time.escudo;
+              escudoImg.src = time.escudo; // elemento escudo do time (adicionando uma imagem armazenada na API)
               escudoImg.alt = nomeTime;
               escudoTd.appendChild(escudoImg);
               tr.appendChild(escudoTd);
 
               const pontosTd = document.createElement('td');
-              pontosTd.textContent = pontos;
+              pontosTd.textContent = pontos; // elemento pontos sendo adicionado a tabela
               tr.appendChild(pontosTd);
 
               const jogosTd = document.createElement('td');
-              jogosTd.textContent = jogos;
+              jogosTd.textContent = jogos; // elemento jogos sendo adicionado a tabela
               tr.appendChild(jogosTd);
 
               const vitoriasTd = document.createElement('td');
-              vitoriasTd.textContent = vitorias;
+              vitoriasTd.textContent = vitorias; // elementos vitórias sendo adicionados a tabela
               tr.appendChild(vitoriasTd);
 
               tbody.appendChild(tr);
           });
 
           table.appendChild(tbody);
-          document.getElementById('tabela').appendChild(table);
+          document.getElementById('tabela').appendChild(table); // elemento na qual todos os dados serão alocados
       })
       .catch(error => {
-          console.error('Erro:', error);
+          console.error('Ocorreu um erro:', error); // caso dê algum erro, será mostrado esta mensagem no log do site
       });
 });
